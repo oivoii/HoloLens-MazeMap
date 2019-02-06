980fd3d89aa75eb4246286e3511a0a2f3a054260
Marius Kohmann <marius.kohmann@gmail.com>
2019-02-06 14:10:25 +0100

    add simple cube script

---
+++ MixedRealityIntroduction/Assets/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+  
+    // Start is called before the first frame update
+    void Start() {
+
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.position = new Vector3(1.0f, 0.0f, 2.0f);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
