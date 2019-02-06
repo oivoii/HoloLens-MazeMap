500fcb5b12547ba4a028c8e23bd3254df71bbabd
Håvard Bjerke <h0wy36@gmail.com>
2019-02-06 16:48:45 +0100

    - Add use of UnityWebRequest to fetch data from MazeMap  - Add some level of JSON parsing, enough to get coordinates

---
+++ MixedRealityIntroduction/Assets/WebGet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using Newtonsoft.Json;
+
+public class WebGet : MonoBehaviour {
+    
+    [System.Serializable]
+    public struct GeometryData
+    {
+        public string type;
+        public List<List<List<double>>> coordinates;
+    }
+    [System.Serializable]
+    public struct MazeMapData
+    {
+        public GeometryData geometry;
+    }
+
+    public string sourceUrl;
+
+    // Use this for initialization
+    void Start() {
+        StartCoroutine(GetData());
+    }
+
+    IEnumerator GetData() {
+        UnityWebRequest webData = UnityWebRequest.Get(sourceUrl);
+
+        yield return webData.SendWebRequest();
+
+        if (webData.isNetworkError || webData.isHttpError) {
+            Debug.LogError(webData.error);
+        }
+
+        var data = JsonConvert.DeserializeObject<MazeMapData>(webData.downloadHandler.text);
+        var geomHeap = data.geometry.coordinates;
+        
+        Debug.Log(geomHeap[1][1].Count);
+    }
+}
